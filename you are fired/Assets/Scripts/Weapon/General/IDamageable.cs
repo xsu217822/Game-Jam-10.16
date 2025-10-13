efe86c655d997f8a631895d5be55735247ff20dc
PhotoerZz <zpy20041015@gmail.com>
2025-10-12 21:52:09 -0400

    pen and damageable class
    damageable class can interact with enemy class

---
+++ you are fired/Assets/Scripts/Weapon/General/IDamageable.cs
@@ -0,0 +1,8 @@
+public interface IDamageable
+{
+    /// <summary>
+    /// damageable接口，表示可以受到伤害的对象
+    /// </summary>
+    /// <param name="amount">伤害</param>
+    void TakeDamage(float amount);
+}
---
+++ you are fired/Assets/Scripts/Weapon/Melee/Pen.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PenWeapon : WeaponBase
+{
+    [Header("Pen Settings")]
+    public float stabDistance = 1.2f;   // 刺击距离
+    public LayerMask hitMask;
+
+    private float lastAttackTime;
+
+    private void Start()
+    {
+        weaponType = WeaponType.Melee;
+    }
+
+    public override void Attack()
+    {
+        if (Time.time < lastAttackTime + GetAttackInterval())
+        {
+            return; // 攻击间隔未到，无法攻击
+        }
+
+        lastAttackTime = Time.time;
+
+        // 播放动画和音效
+        animator?.SetTrigger("Attack");
+        audioSource?.Play();
+
+        // 判定近战攻击
+        Vector2 origin = transform.position;
+        Vector2 direction = transform.right; // 假设角色右方为前方
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, stabDistance, hitMask);
+        if (hit.collider != null)
+        {
+            var target = hit.collider.GetComponent<IDamageable>();
+            if (target != null)
+            {
+                target.TakeDamage(baseDamage);
+            }
+
+            Debug.Log($"[Pen] stabbed {hit.collider.name} for {baseDamage} damage");
+        }
+    }
+}
