bf1510ebaa88b13aa5be696d04ceffc25d47f5d2
PhotoerZz <zpy20041015@gmail.com>
2025-10-12 21:12:45 -0400

    Weaponbase
    creat

--- you are fired/Assets/Scripts/Weapon/WeaponBase.cs
+++ you are fired/Assets/Scripts/Weapon/WeaponBase.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class WeapenBase : MonoBehaviour
+public class WeaponBase : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
