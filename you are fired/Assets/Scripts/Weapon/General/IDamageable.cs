b4078a2ed8a12c822cf25d1e226750ce95dbd280
PhotoerZz <zpy20041015@gmail.com>
2025-10-14 14:51:50 -0400

    Player class
    Add damageable interface

--- you are fired/Assets/Scripts/Weapon/General/IDamageable.cs
+++ you are fired/Assets/Scripts/Weapon/General/IDamageable.cs
@@ -1,8 +1,7 @@
+// Assets/Scripts/Combat/IDamageable.cs
 public interface IDamageable
 {
-    /// <summary>
-    /// damageable接口，表示可以受到伤害的对象
-    /// </summary>
-    /// <param name="amount">伤害</param>
+    bool IsDead { get; }
     void TakeDamage(float amount);
 }
+
