074b0537740ad4c75c5d8e84e57b2f92b4cbe437
Peiyuan Zhao <zpy20041015@gmail.com>
2025-12-27 18:14:53 -0500

    Sword

--- you are fired/Assets/Scripts/Weapon/Melee/Sword.cs
+++ you are fired/Assets/Scripts/Weapon/Melee/Sword.cs
@@ -2,41 +2,42 @@ using UnityEngine;
 
 public class Sword : WeaponBase
 {
+    public float attackRadius = 1.5f;
+    public LayerMask enemyLayer;
 
-    private Enemy cachedTarget;
-    private Animator animator;
-
-    private void Awake()
-    {
-        animator = GetComponentInParent<Animator>();
-    }
+    [Header("Visual FX")]
+    public GameObject slashFxPrefab;
+    public float fxLifeTime = 0.15f;
 
     protected override void PerformAttack(Enemy target)
     {
-        if (!target) return;
+        Vector2 center = transform.position;
 
-        cachedTarget = target;
-
-        animator.SetTrigger("Attack");
-    }
-
-    // ¶¯»­Ö¡
-    public void DoHit()
-    {
-        if (!cachedTarget) return;
-
-        float dist = Vector2.Distance(
-            transform.position,
-            cachedTarget.transform.position
+        Collider2D[] hits = Physics2D.OverlapCircleAll(
+            center,
+            attackRadius,
+            enemyLayer
         );
 
-        if (dist > range) return;
-
-        IDamageable dmg = cachedTarget.GetComponent<IDamageable>();
-        if (dmg != null && !dmg.IsDead)
+        foreach (Collider2D hit in hits)
         {
-            dmg.TakeDamage(baseDamage);
-            Debug.Log("Sword Hit");
+            var dmg = hit.GetComponent<IDamageable>();
+            if (dmg != null && !dmg.IsDead)
+                dmg.TakeDamage(baseDamage);
+        }
+
+        if (slashFxPrefab && target)
+        {
+            Vector2 dir = (target.transform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+            GameObject fx = Instantiate(
+                slashFxPrefab,
+                transform.position,
+                Quaternion.Euler(0, 0, angle)
+            );
+
+            Destroy(fx, fxLifeTime);
         }
     }
 }
